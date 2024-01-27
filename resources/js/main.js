7cda1d049cd40034c972f050ba8b4dbf9b6f8b3a
talhaasre <talhaasre786@gmail.com>
2024-01-27 21:31:05 +0530

    final draft without gallery images

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,5 +1,71 @@
 "use strict";
 
-$(document).ready(function () {
-  $(".loader").css("display", "none");
+document.addEventListener("DOMContentLoaded", function () {
+  const hideLoader = () => {
+    const loader = document.querySelector(".loader");
+    if (loader) {
+      loader.style.display = "none";
+    }
+  };
+
+  const scrollToGallery = (gallerySelector) => {
+    const gallery = document.querySelector(gallerySelector);
+    if (gallery) {
+      gallery.classList.add("active");
+    }
+  };
+
+  const galleryClickHandler = (ctaClass, gallerySelector) => {
+    const ctas = document.querySelectorAll(`.${ctaClass}`);
+    ctas.forEach((cta) => {
+      cta.addEventListener("click", () => scrollToGallery(gallerySelector));
+    });
+  };
+
+  hideLoader();
+
+  galleryClickHandler("phase-I-Cta", ".kh-phase-1-gallery");
+  galleryClickHandler("phase-II-Cta", ".kh-phase-2-gallery");
+
+  document
+    .getElementById("gallery-1-close")
+    .addEventListener("click", function () {
+      document.getElementById("kh-phase-1-gallery").classList.remove("active");
+    });
+  document
+    .getElementById("gallery-2-close")
+    .addEventListener("click", function () {
+      document.getElementById("kh-phase-2-gallery").classList.remove("active");
+    });
+
+  document.getElementById("year").innerHTML = new Date().getFullYear();
 });
+
+function openGallery(boxNumber, galleryNo) {
+  const galleryContent = document.getElementById(`gallery-${boxNumber}`);
+  if (galleryContent) {
+    document.querySelectorAll(".gallery-content").forEach((gallery) => {
+      gallery.style.display = "none";
+    });
+
+    galleryContent.style.display = "block";
+
+    window.scrollTo({
+      behavior: "smooth",
+      top:
+        document.querySelector(galleryNo).getBoundingClientRect().top -
+        document.body.getBoundingClientRect().top -
+        0,
+    });
+  }
+}
+
+const scrollHandler = (selector, offset) => {
+  window.scrollTo({
+    behavior: "smooth",
+    top:
+      document.querySelector(selector).getBoundingClientRect().top -
+      document.body.getBoundingClientRect().top -
+      offset,
+  });
+};
